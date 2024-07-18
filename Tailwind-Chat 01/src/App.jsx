f5f9898e5e899fb8b412a8a72cd9693a3638c13a
Mateus Cimini Araujo <mateusmateus122333@gmail.com>
2024-07-18 14:56:41 -0300

    add new repository
    adicionando o primeiro exercicio de tailwind uma pagina simples com 2 div que foram estilizadas com tailwind

---
+++ Tailwind-Chat 01/src/App.jsx
@@ -0,0 +1,15 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div className="bg-gray-100 flex items-center justify-center h-screen">
+      <div className="bg-white p-6 rounded-lg shadow-lg">
+        <h2 className="text-2xl font-bold mb-2">Olá, Tailwind!</h2>
+        <p className="text-gray-700">Este é um exemplo de um componente simples estilizado com Tailwind CSS.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
